c5ffa792bb8e6ddb1d34eae80c79cbb2f0a31bb2
John Constable <john@kript.net>
2012-02-03 22:36:07 +0000

    tidying in some files

---
+++ bin/OfflineFiles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#script to ensure my home server has a replica of my laptop file
+
+#check we can reach the server, quit if not
+ping -c1 -q  freenas.lab.kript.net > /dev/null #we don't actually want the output
+if  [ $? -ne 0 ]
+then
+#	echo "Can't reach NAS box"
+	exit 1
+else
+	#rsync Documents
+#	 rsync -avz ~/Documents/ john@192.168.1.50:/mnt/fs1/home/john/Documents/ --filter="- ISOs" --filter="- Virtual*"
+	if  [ $? -ne 0 ] then exit 1 fi
+	#rsync Music
+	rsync -avz ~/Music/ john@192.168.1.50:/mnt/fs1/music/
+	if  [ $? -ne 0 ] then exit 1 fi
+	#rsync Pictures
+	rsync -avz ~/Pictures/ john@192.168.1.50:/mnt/fs1/home/john/Pictures/	
+	if  [ $? -ne 0 ] then exit 1 fi
+
+	exit 0
+fi
+
