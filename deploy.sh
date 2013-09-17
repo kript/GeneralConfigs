1dbc98a60aa2196b144aeeb32b09670e9f1fe20c
John Constable <john.constable@sanger.ac.uk>
2013-09-17 12:37:01 +0100

    WIP..

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,9 @@
 
 #VIM spelling customisations
 ln -s ~/Documents/code/GeneralConfigs/.vim/spell/en.utf-8.add ~/.vim/spell/en.utf-8.add
+
+#strat of work to deploy mutt configs
+ln -s ~/Documents/code/GeneralConfigs/mutt/.offlineimaprc ~/
+mkdir -p ~/.mutt
+cp -ra ~/Documents/code/GeneralConfigs/mutt/.mutt ~/.mutt
+
