cbc9eb7050b1dc02d45dd3c6b3b0687e10eba5bd
John Constable <john.constable@sanger.ac.uk>
2013-09-24 09:40:34 +0100

    new ssh cert and more work for the deploy script, such as it is. need to make the deploy script more location independent..

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,5 @@ ln -s ~/Documents/code/GeneralConfigs/.vim/spell/en.utf-8.add ~/.vim/spell/en.ut
 ln -s ~/Documents/code/GeneralConfigs/mutt/.offlineimaprc ~/
 mkdir -p ~/.mutt
 cp -ra ~/Documents/code/GeneralConfigs/mutt/.mutt ~/.mutt
+ln -s ~/Documents/code/GeneralConfigs/mutt/.offlineimaprc ~/
 
